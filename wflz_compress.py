4d0250eb0e7523749f57a0a11bee85cd8310ba5f
AmossGuy <yellowilya@gmail.com>
2026-05-19 20:18:36 -0400

    more wflz_compress skeleton

--- wflz_compress.py
+++ wflz_compress.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import io
+import struct
 
+WFLZ_HEADER_SIZE = 4 * 3
 WFLZ_BLOCK_SIZE = 4
 WFLZ_MIN_MATCH_LEN = WFLZ_BLOCK_SIZE + 1
 
@@ -13,18 +15,36 @@ def wlfz_compress(data):
 	writer = io.BytesIO()
 	
 	# reserve space for header. we won't actually write it until we're finished compressing and know the compressed size
-	writer.write(b"\0" * 4 * 3)
+	writer.write(b"\xFF" * WFLZ_HEADER_SIZE)
 	
-	num_literals = 0
 	wflz_dict = b"\0" * 0xFFFF
+	block_literals = bytearray(b"")
 	
 	# firstly: the first WFLZ_MIN_MATCH_LEN bytes of the data are always written as literals
 	for i in range(min(WFLZ_MIN_MATCH_LEN, len(data))):
 		if len(data) - i >= 4:
-			pass
-		writer.write(data[i].to_bytes(1))
+			pass # TODO: add to dict
+		block_literals.append(data[i])
+	writer.write(struct.pack("<HBB", 0, 0, len(block_literals)))
+	writer.write(block_literals)
 	
-	return writer.getvalue()
+	# TODO: main loop
+	# writer.write(struct.pack("<HBB", ))
+	
+	# TODO: final literals
+	
+	# add terminator block
+	writer.write(b"\0" * WFLZ_BLOCK_SIZE)
+	
+	# write the actual header
+	writer.seek(0)
+	writer.write(b"WFLZ")
+	compressed_size = len(writer.getbuffer()) - (WFLZ_HEADER_SIZE + WFLZ_BLOCK_SIZE)
+	writer.write(struct.pack("<II", compressed_size, len(data)))
+	
+	compressed_data = writer.getvalue()
+	writer.close()
+	return compressed_data
 
 if __name__ == "__main__":
 	print(wlfz_compress(b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer purus elit, vulputate sed vehicula quis, lobortis nec velit. Etiam blandit non est at laoreet. Praesent eleifend dignissim lacus, a auctor tortor dignissim sit amet. In ut ornare diam, ut molestie nisl. Suspendisse in elementum lorem, ut ullamcorper ipsum. Quisque libero leo, ultricies at maximus non, pellentesque non erat. Praesent convallis tincidunt mollis."))
