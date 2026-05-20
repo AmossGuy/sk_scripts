800a9a077376f0a259abd3a23c1104afa01815cc
AmossGuy <yellowilya@gmail.com>
2026-05-19 20:33:06 -0400

    add final literals
    i think this is enough to get output that is correct but not actually compressed

--- wflz_compress.py
+++ wflz_compress.py
@@ -5,7 +5,9 @@ import struct
 
 WFLZ_HEADER_SIZE = 4 * 3
 WFLZ_BLOCK_SIZE = 4
+
 WFLZ_MIN_MATCH_LEN = WFLZ_BLOCK_SIZE + 1
+WFLZ_MAX_LITERALS = 0xFF
 
 def wlfz_hashptr(x):
 	# the original wflz source uses some fancy macros to calculate that 16, but in the end it's just a constant
@@ -17,21 +19,33 @@ def wlfz_compress(data):
 	# reserve space for header. we won't actually write it until we're finished compressing and know the compressed size
 	writer.write(b"\xFF" * WFLZ_HEADER_SIZE)
 	
+	read_pos = 0
 	wflz_dict = b"\0" * 0xFFFF
 	block_literals = bytearray(b"")
 	
 	# firstly: the first WFLZ_MIN_MATCH_LEN bytes of the data are always written as literals
-	for i in range(min(WFLZ_MIN_MATCH_LEN, len(data))):
-		if len(data) - i >= 4:
+	for _ in range(min(WFLZ_MIN_MATCH_LEN, len(data))):
+		if len(data) - read_pos >= 4:
 			pass # TODO: add to dict
-		block_literals.append(data[i])
+		block_literals.append(data[read_pos])
+		read_pos += 1
 	writer.write(struct.pack("<HBB", 0, 0, len(block_literals)))
 	writer.write(block_literals)
+	block_literals = bytearray(b"")
 	
 	# TODO: main loop
 	# writer.write(struct.pack("<HBB", ))
 	
-	# TODO: final literals
+	# final literals
+	while read_pos < len(data):
+		if len(block_literals) == WFLZ_MAX_LITERALS:
+			writer.write(struct.pack("<HBB", 0, 0, len(block_literals)))
+			writer.write(block_literals)
+			block_literals = bytearray(b"")
+		block_literals.append(data[read_pos])
+		read_pos += 1
+	writer.write(struct.pack("<HBB", 0, 0, len(block_literals)))
+	writer.write(block_literals) # no need to erase it this time since we're already done
 	
 	# add terminator block
 	writer.write(b"\0" * WFLZ_BLOCK_SIZE)
