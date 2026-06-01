77fde6048c0a60aec85d8b6fc3dac7978eed0334
AmossGuy <yellowilya@gmail.com>
2026-06-01 15:14:07 -0400

    create new wrapper script for extraction

---
+++ sk_extract.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from pathlib import Path
+
+parser = ArgumentParser(description="extracts various file formats used by Shovel Knight into editable forms")
+parser.add_argument("filename")
+
+args = parser.parse_args()
+source_filename = Path(args.filename)
+
+match source_filename.suffix:
+	case ".ltb":
+		from sk_include.ltb_explode import ltb_explode
+		ltb_explode(source_filename)
+	case _:
+		print("unknown file extension. this script relies on file extensions to identify file type")
+		sys.exit(1)
